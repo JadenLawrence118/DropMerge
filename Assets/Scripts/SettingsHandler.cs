a2c46cac31293b6901fa4036d11745e367a907c3
Jaden Lawrence <jaden.lawrence11@gmail.com>
2025-06-29 18:32:08 +0100

    Audio slider update
    Update the appearance of the audio sliders, and made them return to the correct value when the settings screen is loaded

--- Assets/Scripts/SettingsHandler.cs
+++ Assets/Scripts/SettingsHandler.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class SettingsHandler : MonoBehaviour
 {
     public GameObject confirmWindow;
     public GameObject OKWindow;
     public TextMeshProUGUI HighScore;
+    public UnityEngine.UI.Slider musicSlider;
+    public UnityEngine.UI.Slider SFXSlider;
 
     private void Awake()
     {
         confirmWindow.SetActive(false);
         OKWindow.SetActive(false);
         HighScore.text = "High Score: " + PlayerPrefs.GetInt("HighScore");
+        musicSlider.value = PlayerPrefs.GetFloat("musicVol", 1.0f);
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVol", 1.0f);
     }
 }
