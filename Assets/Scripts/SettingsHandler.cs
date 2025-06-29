bceb503c47f9765bd810b95ad956f427e96af4d4
Jaden Lawrence <jaden.lawrence11@gmail.com>
2025-06-29 17:42:56 +0100

    Added high score to settings
    Added the player's high score to the settings screen to allow them to see it before choosing to reset it

--- Assets/Scripts/SettingsHandler.cs
+++ Assets/Scripts/SettingsHandler.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SettingsHandler : MonoBehaviour
 {
     public GameObject confirmWindow;
     public GameObject OKWindow;
+    public TextMeshProUGUI HighScore;
 
     private void Awake()
     {
         confirmWindow.SetActive(false);
         OKWindow.SetActive(false);
+        HighScore.text = "High Score: " + PlayerPrefs.GetInt("HighScore");
     }
 }
